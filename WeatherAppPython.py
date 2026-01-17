22a14a2f71c742569ceb6acd2559f0137084dcff
Aditya Kinhikar <adityakinhikar1@com>
2026-01-17 20:37:25 +0000

    Add loop and improve weather app handling

--- WeatherAppPython.py
+++ WeatherAppPython.py
@@ -1,24 +1,31 @@
 
 import requests
 
-API_KEY = "Open_Weather_API_KEY"
-city = input("Enter city name: ").strip()
+API_KEY = "082f8bbf4bffd19402ae3d6ffa37773d"
 
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+print("Welcome to Weather Forecast")
 
-response = requests.get(url)
-data = response.json()
+while True:
+    city = input("Please Enter city Name (Or type end to quit): ").strip()
+    
+    if city.lower() == "end":
+        print("Exiting the Weather Forecast!")
+        break
 
-if response.status_code == 200:
-    temperature = data["main"]["temp"]
-    humidity    = data["main"]["humidity"]
-    description = data["weather"][0]["description"]
-    wind_speed  = data["wind"]["speed"]
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    data = response.json()
 
-    print(f"\nWeather Report for {city}:")
-    print(f"Temperature : {temperature}°C")
-    print(f"Humidity    : {humidity}%")
-    print(f"Condition   : {description}")
-    print(f"Wind Speed  : {wind_speed} m/s")
-else:
-    print("City not found!")
+    if response.status_code == 200:
+      temperature = data["main"]["temp"]
+      humidity    = data["main"]["humidity"]
+      description = data["weather"][0]["description"]
+      wind_speed  = data["wind"]["speed"]
+
+      print(f"\nWeather Report for {city}:")
+      print(f"Temperature : {temperature}°C")
+      print(f"Humidity    : {humidity}%")
+      print(f"Condition   : {description}")
+      print(f"Wind Speed  : {wind_speed} m/s")
+    else:
+      print("City not found or Input Error.. Please Try Again")
